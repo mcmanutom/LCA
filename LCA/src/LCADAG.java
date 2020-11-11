bcd92366dbf9a293ba7ba0e3b67739258cece6a3
user <user@10.32.110.122>
2020-11-11 20:32:42 +0000

    small functions that are required

--- LCA/src/LCADAG.java
+++ LCA/src/LCADAG.java
@@ -19,4 +19,48 @@ public class LCADAG {
 			list2[x] = new ArrayList<Integer>();
 		}
 	}
+	
+	public boolean addEdge(int a, int b)
+	{
+		//Make sure vertexes are both in range (not less than 0 or bigger than number of vertexes in the DAG)
+		if(a >= this.x || b >= this.x || a < 0 || b < 0)
+		{
+			return false;
+		}
+		if(a != b && !hasPath(a, b) && !list1[a].contains(b))
+		{
+			list1[a].add(b);
+			list2[a].add(b);
+			return true;
+		}	
+		else
+		{
+			return false;
+		}
+	}
+	
+	//Returns number of vertexes in the graph
+	public int vertexes()
+	{
+		return x;
+	}
+
+	//Returns list of vertices pointing from the vertex y 
+	public ArrayList<Integer> list1(int y)
+	{ 
+		return list1[y];
+	}
+
+	//Returns reversed list of vertices pointing from the vertex y 
+	public ArrayList<Integer> reverseAdj(int y)
+	{
+		return list2[y];
+	}
+	
+
+	//Checks if a path exists between two nodes 
+	public boolean hasPath(int a, int b)
+	{
+		
+	}
 }
